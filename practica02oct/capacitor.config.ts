1ce2747971168eb9ea0dc2746a5c447c6e881bbe
marioodysey64-cyber <marioodysey64@gmail.com>
2025-10-02 11:59:59 -0600

    cree el proyecto para la semana 8

---
+++ practica02oct/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'practica02oct',
+  webDir: 'www'
+};
+
+export default config;
